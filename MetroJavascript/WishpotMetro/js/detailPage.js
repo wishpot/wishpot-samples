aad569344175f0b1d5cebfed2e34dda39a40a641
Tom Lianza <tom@lianza.org>
2012-03-01 00:21:03 -0800

    Initial WishpotMetro sample.

---
+++ MetroJavascript/WishpotMetro/js/detailPage.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    'use strict';
+
+    // Custom event raised after the fragment is appended to the DOM.
+    WinJS.Application.addEventListener('fragmentappended', function handler(e) {
+        if (e.location === '/html/detailPage.html') { fragmentLoad(e.fragment, e.state); }
+    });
+
+    function fragmentLoad(elements, options) {
+        var item = options && options.item ? options.item : getItem();
+        elements.querySelector('.pageTitle').textContent = item.group.title;
+
+        WinJS.UI.processAll(elements)
+            .then(function () {
+                elements.querySelector('.title').textContent = item.product.title;
+                elements.querySelector('.content').innerHTML = item.product.description;
+                elements.querySelector('.itemImage').src = item.product.largePicture;
+                elements.querySelector('.buy').onclick = function () {
+                    window.open(item.product.url);
+                }
+                elements.querySelector('.add').onclick = function () {
+                    WPJS.EnsureAuth(function (result) {
+                        
+                    });
+                }
+            });
+    }
+
+    // The getItem() function contains sample data.
+    // TODO: Replace with custom data.
+    function getItem() {
+        //PROBLEM!  Should never need to call this.
+    }
+
+    WinJS.Namespace.define('detailPage', {
+        fragmentLoad: fragmentLoad,
+    });
+})();
---
+++ MetroJavascript/WishpotMetro/js/wpjs.js
@@ -0,0 +1,427 @@
+﻿WISHPOT_HOSTNAME = "www.wishpot.com"; //"main.test.wishpot.com"; 
+WISHPOT_IMG_ROOT = "//" + WISHPOT_HOSTNAME + "/img/";
+PRODUCT_PORTALS_KEY = "558";
+//FACEBOOK_APP_ID = "3003981649"; 
+FACEBOOK_APP_ID = "2456371452";
+
+var WPJS = {
+    BaseUri: WISHPOT_HOSTNAME,
+
+    Consumer:
+    {
+        consumerKey: "WindowsTablet"
+        , consumerSecret: "ddfd6d1e1e8a43e8a41a5581bfd7439c"
+        , userKey: null
+        , userSecret: null
+        , serviceProvider:
+        {
+            signatureMethod: "HMAC-SHA1"
+                , requestTokenURL: "http://" + WISHPOT_HOSTNAME + "/api/RequestToken.ashx "
+                , userAuthorizationURL: "https://" + WISHPOT_HOSTNAME + "/secure/signin.aspx"
+                , accessTokenURL: "http://" + WISHPOT_HOSTNAME + "/api/AccessToken.ashx"
+        },
+        isLoggedIn: function () {
+            return (WPJS.Consumer.userKey != null && WPJS.Consumer.userSecret != null);
+        },
+
+        generateAccess: function () {
+            return { consumerSecret: WPJS.Consumer.consumerSecret, tokenSecret: WPJS.Consumer.userSecret };
+        },
+
+        //'params' should be an array of arrays, where each array has two entries: a name and a value
+        generateBaseConsumerMessage: function (apiPath, method, params) {
+            var p = [['oauth_signature_method', WPJS.Consumer.serviceProvider.signatureMethod]
+                   , ['oauth_consumer_key', WPJS.Consumer.consumerKey]];
+
+            if (null != WPJS.Consumer.userKey)
+                p = p.concat([['oauth_token', WPJS.Consumer.userKey]]);
+
+            if (null != params)
+                p = p.concat(params);
+
+            return {
+                action: "http://" + WPJS.BaseUri + apiPath
+              , method: method
+              , parameters: p
+            };
+        },
+
+        generateFinalUrl: function (message) {
+            OAuth.setTimestampAndNonce(message);
+            OAuth.completeRequest(message, WPJS.Consumer.generateAccess());
+            return OAuth.addToURL(message.action, message.parameters);
+        }
+    },
+
+    PPConsumer:
+    {
+        getSearchUri: function (term) {
+            return "http://productportals.wishpot.com/product_api.xml?term=" + term + "&placement_id=" + PRODUCT_PORTALS_KEY;
+        }
+    },
+
+    //the lists of the logged in user
+    UserLists : {},
+
+    Debug: function (msg) {
+        if (typeof(console) != "undefined" && console.log) { console.log(msg); }
+    },
+
+    AddWish: function(productId, productSource)
+    {
+        if (!WPJS.Consumer.isLoggedIn()) {
+            return false;
+        }
+
+    },
+
+    //Launches an auth dialog if necessary.  calls back with 'true' or 'false' depending on whether or now the user has auth'd 
+    EnsureAuth: function (callback) {
+        if (WPJS.Consumer.isLoggedIn()) {
+            callback(true); return;
+        }
+        WPJS._launchFacebookWebAuth(function (token){ WPJS.TokenExchange(token, callback); });
+    },
+
+    TokenExchange: function (facebookToken, callback) {
+        if (null == facebookToken)
+            callback(false);
+
+        var message = WPJS.Consumer.generateBaseConsumerMessage("/restapi/User/OAuthCredential/Exchange/2", "POST", [["OAuthCredential.AccessToken", facebookToken]]);
+        var url = WPJS.Consumer.generateFinalUrl(message);
+        WinJS.xhr({ url: url, type: message.method }).then(
+        function (result) {
+            WPJS._StoreUserToken(result.responseXML, callback);
+            WPJS._StoreUserLists();
+        },
+        function (result) {
+            console.log("Error: " + result.status);
+            callback(false);
+        });
+    },
+
+    _StoreUserToken: function (resultXml, callback) {
+        var token = resultXml.selectNodes("AuthToken");
+        WPJS.Consumer.userKey = WPJS.XML.safeParseXmlText(token[0], "Token", null);
+        WPJS.Consumer.userSecret = WPJS.XML.safeParseXmlText(token[0], "TokenSecret", null);
+        callback(true);
+    },
+
+    _StoreUserLists: function()
+    {
+        if (!WPJS.Consumer.isLoggedIn()) {
+            return false;
+        }
+        var message = WPJS.Consumer.generateBaseConsumerMessage("/restapi/User/Lists", "GET");
+        var url = WPJS.Consumer.generateFinalUrl(message);
+        
+        WinJS.xhr({ url: url, type: message.method, headers: { "Accept": "application/json" } }).then(
+        function (result) {
+            WPJS.UserLists = JSON.parse(result.response)["Lists"];
+        },
+        function (result) {
+            console.log("Error getting user lists: " + result.status);
+        });
+    },
+
+    _launchFacebookWebAuth: function (callback) {
+        var facebookURL = "https://www.facebook.com/dialog/oauth?client_id=";
+        var clientID = FACEBOOK_APP_ID;
+        var callbackURL = "https://www.facebook.com/connect/login_success.html";
+        facebookURL += clientID + "&redirect_uri=" + encodeURIComponent(callbackURL) +
+ "&scope=publish_actions,user_birthday,user_likes,email&display=popup&response_type=token";
+
+        try {
+            var startURI = new Windows.Foundation.Uri(facebookURL);
+            var endURI = new Windows.Foundation.Uri(callbackURL);
+
+            Windows.Security.Authentication.Web.WebAuthenticationBroker.authenticateAsync(
+			Windows.Security.Authentication.Web.WebAuthenticationOptions.default,
+			startURI,
+			endURI).then(function (result){ WPJS._callbackFacebookWebAuth(result, callback); }, WPJS.callbackFacebookWebAuthError);
+        }
+        catch (err) {	/*Error launching WebAuth"*/	return; }
+    },
+
+    _callbackFacebookWebAuth: function (result, callback) {
+        var FacebookReturnedToken = result.responseData;
+        var response = "Status returned by WebAuth broker: " + result.responseStatus + "\r\n";
+        if (result.responseStatus == 2) {
+            response += "Error returned: " + result.responseErrorDetail + "\r\n";
+        }
+        var hashPos = FacebookReturnedToken.indexOf("#");
+        if (hashPos <= 0)
+            callback(null); //we did not get a valid token
+
+        var tokenParamName = "access_token="
+        var tokenPos = FacebookReturnedToken.indexOf(tokenParamName, hashPos);
+        if (tokenPos <= 0)
+            callback(null); //again, no valid token
+
+        callback(FacebookReturnedToken.substring(tokenPos + tokenParamName.length, FacebookReturnedToken.indexOf("&", tokenPos)));
+    },
+
+    _callbackFacebookWebAuthError: function (err) {
+        var error = "Error returned by WebAuth broker. Error Number: " + err.number + " Error Message: " + err.message + "\r\n";
+        WPJS.Debug(error);
+    },
+
+    initFluidLists: function (listSelector) {
+        jQuery(function ($) {
+            var listSelector = listSelector ? listSelector : 'ol.fluid:visible';
+            $(listSelector).each(function () {
+                var mainImageArray = $(this).find('div.top a.image img');
+                var ownerImagegArray = $(this).find('div.owner a.image img');
+                // Set container height
+                var listHeightMultiple = $(this).attr('data-height-multiple') || 1;
+                $(mainImageArray).each(function () {
+                    var objRoot = $(this).closest('li');
+                    var objParent = $(this).parent();
+                    var objParentOriginalWidth = objParent.width();
+                    var objWidthMultiple = objRoot.attr('data-width-multiple') || 1;
+                    if (objWidthMultiple != 1) {
+                        objRoot.css('width', (objRoot.width() * objWidthMultiple) + 'px');
+                    }
+                    var objHeightMultiple = objRoot.attr('data-height-multiple') || listHeightMultiple;
+                    objParent.css('height', Math.round(objParentOriginalWidth * objHeightMultiple) + 'px');
+                });
+                $(ownerImagegArray).each(function () {
+                    $(this).parent().css('height', Math.round($(this).parent().width()) + 'px');
+                });
+                // Resize images
+                var imageArray = mainImageArray.add(ownerImagegArray);
+                if ($(this).hasClass('scaletofit')) {
+                    scaleToFit($(imageArray));
+                }
+                else {
+                    scaleToFill($(imageArray));
+                }
+                // Attach Events: Slider
+                if ($(this).hasClass('slideinfo')) {
+                    $('li', this).bind('mouseenter.slider', function () {
+                        $('div.bottom', this).stop().css('bottom', '-100px').animate({ 'bottom': '0' }, { queue: false, duration: 200 });
+                    }).bind('mouseleave.slider', function () {
+                        $('div.bottom', this).stop().animate({ 'bottom': '-100px' }, { queue: false, duration: 200 });
+                    });
+                }
+            });
+        })
+    },
+
+    scaleToFill: function (imgSelector, fallbackScaleToFit) {
+        jQuery(function ($) {
+            $(imgSelector).not('img.noimage').each(function () {
+                $(this).parent().css({
+                    'position': 'relative',
+                    'overflow': 'hidden'
+                });
+                $(this).css({
+                    'position': 'absolute',
+                    'height': 'auto',
+                    'width': 'auto'
+                });
+                var objHeight = $(this).height();
+                var objWidth = $(this).width();
+
+                //this can happen when an image hasn't downloaded yet.  If so, go back and scale it when it's ready
+                if (WPJS.imageIsNotLoaded(this)) {
+                    WPJS.Debug("Image hadn't loaded when we tried to scale it: " + $(this).attr("src"));
+                    $(this).load(function () { scaleToFill(this, fallbackScaleToFit); });
+                    return true;
+                }
+                //WPJS.Debug("Scaling to fill: " + $(this).attr("src"));
+
+                var parentHeight = $(this).parent().height();
+                var parentWidth = $(this).parent().width();
+
+                if (fallbackScaleToFit && (objHeight * 2 < parentHeight && objWidth * 2 < parentWidth) || objHeight / objWidth > 2 || objWidth / objHeight > 2) {
+                    WPJS.Debug("Doing a scaletofit because image was too small or oddly-shaped (" + objHeight + "x" + objWidth + "): " + this + " " + $(this).attr("src"));
+                    return scaleToFit(this);
+                }
+
+                var heightDiff = parentHeight - objHeight;
+                var widthDiff = parentWidth - objWidth;
+                if (heightDiff >= widthDiff) {
+                    $(this).css('height', parentHeight + 'px');
+                }
+                else if (widthDiff > heightDiff) {
+                    $(this).css('width', parentWidth + 'px');
+                }
+                $(this).css({
+                    'top': Math.round((parentHeight - $(this).height()) / 2) + 'px',
+                    'left': Math.round((parentWidth - $(this).width()) / 2) + 'px',
+                    'visibility': 'visible'
+                });
+            });
+        })
+    },
+
+    scaleToFit: function (imgSelector) {
+        jQuery(function ($) {
+            $(imgSelector).not('img.noimage').each(function () {
+                $(this).parent().css({
+                    'position': 'relative',
+                    'overflow': 'hidden'
+                });
+                $(this).css({
+                    'position': 'absolute',
+                    'height': 'auto',
+                    'width': 'auto'
+                });
+                var objHeight = $(this).height();
+                var objWidth = $(this).width();
+
+                //this can happen when an image hasn't downloaded yet.  If so, go back and scale it when it's ready
+                if (WPJS.imageIsNotLoaded(this)) {
+                    WPJS.Debug("Image hadn't loaded when we tried to scale it: " + $(this).attr("src"));
+                    $(this).load(function () { scaleToFit(this); });
+                    return true;
+                }
+
+                var parentHeight = $(this).parent().height();
+                var parentWidth = $(this).parent().width();
+                var heightDiff = parentHeight - objHeight;
+                var widthDiff = parentWidth - objWidth;
+
+                if (heightDiff >= widthDiff) {
+                    $(this).css('width', parentWidth + 'px');
+                }
+                else if (widthDiff > heightDiff) {
+                    $(this).css('height', parentHeight + 'px');
+                }
+                $(this).css({
+                    'top': Math.round((parentHeight - $(this).height()) / 2) + 'px',
+                    'left': Math.round((parentWidth - $(this).width()) / 2) + 'px',
+                    'visibility': 'visible'
+                })
+            });
+        })
+    },
+
+    //Runs through images with the specific class and swaps them if they aren't loaded
+    correctErroredImages: function () {
+        jQuery('.handleError > img').each(function () {
+            if (WPJS.imageIsNotLoaded(this)) swapImageForError(this);
+        });
+    },
+
+    swapImageForError: function (imageSelector) {
+        jQuery(function ($) {
+            WPJS.Debug("Error handler called for: " + $(imageSelector).attr("src"));
+            if ($(imageSelector).hasClass('wishPic') || $(imageSelector).hasClass('listPic'))
+                $(imageSelector).attr('src', WISHPOT_IMG_ROOT + '/item_100.gif');
+            else if ($(imageSelector).hasClass('userPic'))
+                $(imageSelector).attr('src', WISHPOT_IMG_ROOT + '/nopicusr.gif');
+            else if ($(imageSelector).hasClass('prodPic'))
+                $(imageSelector).attr('src', WISHPOT_IMG_ROOT + '/item_150.gif');
+            else
+                $(imageSelector).attr('src', WISHPOT_IMG_ROOT + '/nophoto160x160.gif');
+
+            //if the parent has height, fill it (some parents have a tiny bit of height 'cause of some text)
+            if ($(imageSelector).parent().height() > 15)
+                scaleToFill(imageSelector);
+            else
+                $(imageSelector).css({ 'height': 'auto', 'width': 'auto' });
+        })
+    },
+    imageIsNotLoaded: function (imgObj) {
+        //WPJS.Debug("Checking if image has loaded: " + $(imgObj).attr("src"));
+        if (typeof (imgObj.naturalWidth) != "undefined") return (imgObj.naturalWidth == 0);
+        if (typeof (imgObj.readyState) != "undefined") return (imgObj.readyState == 'uninitialized');
+        return (jQuery(imgObj).width() == 0 || jQuery(imgObj).height() == 0);
+    },
+
+    Categories: function () {
+        return WPJS.LocalCache.get("wishpotCategories") || [];
+    },
+
+    /* AJAX FUNCTIONS */
+    loadCategories: function () {
+        if (WPJS.Categories().length > 0) return;
+
+        WinJS.xhr({ url: "http://" + WISHPOT_HOSTNAME + "/restapi/Category" }).then(
+            function (result) {
+            WPJS._loadCategoryXml(result);
+        },
+        function (result) {
+            console.log("Error: " + result.status);
+        });
+    },
+
+    _loadCategoryXml: function (result) {
+        var xml = result.responseXML;
+        var items = xml.selectNodes("Categories/Category");
+        if (items) {
+            var length = Math.min(5000, items.length);  //hard-coded max to prevent potential bugs
+            var categories = [];
+            for (var i = 0; i < length; i++) {
+                categories[items[i].selectSingleNode("Id").text] = items[i].selectSingleNode("Name").text;
+            }
+            WPJS.LocalCache.set("wishpotCategories", categories, 60);
+        }
+    },
+
+    LocalCache:
+    {
+        get: function (key) {
+            var o = localStorage.getItem(key);
+            if (o == null) return null;
+
+            o = JSON.parse(o);
+
+            //delete if expired
+            if (o.timestamp + o.expiration < new Date().getTime()) {
+                WPJS.Debug("Deleting " + key + " from cache, as it has expired.");
+                localStorage.removeItem(key);
+                return null;
+            }
+            return o.value;
+        },
+        set: function (key, value, expirationInMinutes) {
+            var o = { value: value, timestamp: new Date().getTime(), expiration: expirationInMinutes * 60 * 1000 }
+            localStorage.setItem(key, JSON.stringify(o));
+        }
+    },
+
+    XML:
+    {
+        safeParseXmlText: function (xml, nodePath, defaultIfNotPresent) {
+            var node = xml.selectNodes(nodePath);
+            if(node != null && node[0] != null)
+                return node[0].text;
+            return defaultIfNotPresent;
+        }
+    }
+}
+
+/* Simple Product object */
+function Product() {
+
+}
+(function () {
+    
+    /*
+    TODO: build out a real product object... unfortunately curently doesn't work with windows databinding 
+    function setPictures(pics) {
+        this.pictures = pics;
+    }
+    
+    function getSmallPic() {
+        return (this.pictures.length > 0) ? this.pictures[0] : null;
+    }
+   
+    function getLargePic() {
+        return (this.pictures.length > 1) ? this.pictures[1] : this.getSmallPic();
+    }
+
+    Product.prototype = {
+        largePicture: getLargePic,
+        picture: getSmallPic,
+        setPictures: setPictures
+    };
+    */ 
+})();
+
+
+
+
